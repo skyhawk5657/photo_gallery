9d9ff553e8b780b2bad42dc836b0b3f3ea2b3a9d
skyhawk5657 <skyhawk5657@gmail.com>
2015-09-11 01:04:25 -0700

    v 0.30 build gallery with Bootstrap grids

--- index.php
+++ index.php
@@ -10,16 +10,38 @@
 require_once('config.php');
 require_once('functions.php');
 
-$page_title = "Photo Gallery";
-
- // Include the header template:
-require_once('header.php');
-
-/******* Setting & HTML generation *******/
 /** settings **/
-$images_dir = 'gallery/201506_leoPark/';
-$thumbs_dir = 'gallery/201506_leoPark/thumbs/';
 $thumbs_width = 200;
+$page_title = 'Photo Gallery';
+$images_dir = 'gallery/201506_leoPark/';
+$thumbs_dir = $images_dir .'thumbs/';
+$album_title = 'Leo J. Ryan Memorial Park<br>San Mateo, CA';
+
+// build Gallery HTML codes
+$html_gc = ''; 
+$image_files = get_files($images_dir);
+sort($image_files); // sort file name in ascending order
+if(count($image_files)) {
+	$index = 0;
+	foreach($image_files as $index=>$file) {
+		$index++;
+		$thumbnail_image = $thumbs_dir.$file;
+		if(!file_exists($thumbnail_image)) {
+			$extension = get_file_extension($thumbnail_image);
+			if($extension) {
+				make_thumb($images_dir.$file,$thumbnail_image,$thumbs_width);
+			}
+		}
+		$html_gc .= "<div class=\"col-xs-12 col-sm-6 col-md-4 col-lg-3\"><div class=\"element\"><a href=\"".$images_dir.$file."\" class=\"fancybox-button\" rel=\"fancybox-button\" title=\"".$file."\"><img src=\"".$thumbnail_image."\" /></a></div></div>\r\n";
+	}
+	$html_gc .= "\r\n";
+}
+else {
+	$html_gc = '<p>There are no images in this gallery.</p>';
+}
+					
+// Include the header template:
+require_once('header.php');
 ?>
 
 <body>
@@ -37,33 +59,13 @@ $thumbs_width = 200;
 		</header>
 
 		<main>
-			<div class="row">
-				<div class="gallery">
-					<p class="album-title">Leo J. Ryan Memorial Park<br>San Mateo, CA</p>
-					<?php /** generate photo gallery **/
-					$html_gc = ''; // initialize and start building Gallery HTML codes
-					$image_files = get_files($images_dir);
-					sort($image_files); // sort file name in ascending order
-					if(count($image_files)) {
-						$index = 0;
-						foreach($image_files as $index=>$file) {
-							$index++;
-							$thumbnail_image = $thumbs_dir.$file;
-							if(!file_exists($thumbnail_image)) {
-								$extension = get_file_extension($thumbnail_image);
-								if($extension) {
-									make_thumb($images_dir.$file,$thumbnail_image,$thumbs_width);
-								}
-							}
-							$html_gc .= "<div class=\"element\"><a href=\"".$images_dir.$file."\" class=\"fancybox-button\" rel=\"fancybox-button\" title=\"".$file."\"><img src=\"".$thumbnail_image."\" /></a></div>\r\n";
-						}
-						$html_gc .= "\r\n";
-						echo $html_gc;
-					}
-					else {
-						echo '<p>There are no images in this gallery.</p>';
-					}
-					?>
+			<div class="gallery">
+				<p class="album-title"><?php echo $album_title; ?></p>
+				<div class="row">					
+					
+						<?php echo $html_gc; ?>
+						
+					</div>
 				</div><!-- /gallery -->				
 			</div><!-- /row -->
 		</main>
