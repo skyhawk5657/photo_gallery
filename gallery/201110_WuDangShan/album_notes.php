a57a513c4b8fbfe7dea5f619273023426c71ff93
skyhawk5657 <skyhawk5657@gmail.com>
2015-09-24 11:15:45 -0700

    move file to proper folder

---
+++ gallery/201110_WuDangShan/album_notes.php
@@ -0,0 +1,5 @@
+<?php
+$album_title = "武當山 後山  ---> <a href='http://www.hyview.com/photo_gallery/?an=201110_WuDangShan_2'>前山</a>";
+$album_date = "10/20111";
+$album_description = "<p>點閱方式: 點第一張相片, 或選任何一張相片, 它會放大,<br />在放大的相片裡, 點右邊, 看下一張, <br />點左邊, 到前一張.</p><p>iOS (iPhone, iPad)機子的可能要點兩次,<br />點第一次讓箭頭顯示出來, 點第二次才換下或上一張.</p><p>本相簿中的相片拍攝於 2011 年 10 月 下旬</p><p>由三部相機所攝收集而成, 故解析度, 品質不一, 請多包涵</p>";
+?>
--- gallery/album_notes.php
+++
@@ -1,5 +0,0 @@
-<?php
-$album_title = "Leo J. Ryan Memorial Park<br>San Mateo, CA";
-$album_date = "6/2015";
-$album_description = "Photos were taken in 6/2015 at the Leo Park in San Mateo, CA";
-?>
